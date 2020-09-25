0c19ed40abb5553ec983327b70575d08585585e2
RISHI_BHARADWAJ <rishi.r.bharadwaj@gmail.com>
2020-09-25 22:10:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+//Create variables here
+var dog, happyDog;
+var database;
+var foodS , foodStock;
+
+function preload()
+{
+  //load images here
+  dog = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png");
+}
+
+function setup() 
+{
+  createCanvas(500, 500);
+  dog1 = createSprite(250,300,50,50);
+  dog1.addImage("dogImg",dog);
+  dog1.scale = 0.2;
+
+  foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+  
+ 
+  
+}
+
+
+function draw() 
+{  
+  background(rgb(46, 139, 87));
+  if(keyWentDown(UP_ARROW))
+  {
+   writeStock(foodS);
+   dog.addImage("dogImg",happyDog);
+  }
+
+  drawSprites();
+  //add styles here
+  textSize(20);
+  fill("white");
+  stroke();
+  text("Note: Press UP_ARROW Key To Feed Spikey!!",100,200);
+
+}
+function readStock(data)
+{
+  foodS = data.val();
+}
+
+function writeStock(x)
+{
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
+
